9b663bd73bac470f23690a602c9d5d3e55db825c
Karl Molina <karlgmolina@gmail.com>
2019-08-05 15:00:03 -0600

    Update dotfiles

--- .dataminr_aliases.sh
+++ .dataminr_aliases.sh
@@ -1,4 +1,9 @@
 alias vda='vi ~/.dataminr_aliases.sh'
+
 alias et='repos; cd enterprise-management-testing'
 alias vl='repos; cd dm-vigil'
 alias listlinks='ls -l node_modules | grep ^l'
+
+# npm links
+alias lkvl='npm link $REPOS/dm-vigil'
+alias ulvl='npm unlink $REPOS/dm-vigil'
