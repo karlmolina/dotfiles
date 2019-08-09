19f7072d4a234b13a1a4705bf2acb76ca184d8ff
Karl Molina <karlgmolina@gmail.com>
2019-08-09 11:00:52 -0600

    Update dotfiles

--- .dataminr_aliases.sh
+++ .dataminr_aliases.sh
@@ -4,6 +4,7 @@ alias sda='vi ~/.dataminr_aliases.sh'
 alias et='repos; cd enterprise-management-testing'
 alias vl='repos; cd dm-vigil'
 alias ca='repos; cd ca-testing'
+alias st='repos; cd ghost-testing'
 
 
 alias listlinks='ls -l node_modules | grep ^l'
@@ -13,3 +14,10 @@ alias lkvl='npm link $REPOS/dm-vigil'
 alias ulvl='npm unlink $REPOS/dm-vigil'
 
 alias v='echo "npm@$(npm -v) node@$(node -v)"'
+
+
+alias runet='grunt --files="tests/end-to-end/client-app-first-alert-dark-hardcoded.js"'
+alias runca='grunt --files="tests/corporate/historical/apply-searches/edit-searches/foreign-language.js"'
+
+
+alias installvl='npm install ../dm-vigil'
