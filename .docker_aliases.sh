552a8beb96a065e2e86ff5c74364585e06113ca6
Karl Molina <karlgmolina@gmail.com>
2019-12-20 09:28:45 -0700

    Update dotfiles

--- .docker_aliases.sh
+++ .docker_aliases.sh
@@ -1,7 +1,11 @@
 alias voa='vi ~/.docker_aliases.sh'
 
+alias oo='open -a "Docker 2"
+
 alias ds='docker ps'
 alias dt='docker start'
 alias dtg='docker start gitlab'
 alias dtj='docker start jenkins'
 alias dl='docker logs -f'
+
+
