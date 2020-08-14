e8cfe2868246a902b60738b971085f5209c97a4e
Karl Molina <karlgmolina@gmail.com>
2020-08-14 12:59:53 -0600

    Update dotfiles

--- .docker_aliases.sh
+++ .docker_aliases.sh
@@ -27,3 +27,14 @@ startgitlab () {
       --rm \
       gitlab/gitlab-ce:latest
 }
+
+
+startjenkinsagent () {
+    docker run -d --name jenkins-agent \
+    --rm --init jenkins/inbound-agent \
+    -url http://host.docker.internal:49001 \
+    # -workDir=~/Documents/jenkins \
+    -v /Users/kmolina/Documents/jenkins:/var/jenkins_home \
+    241eca8ab5f337caef8869b261eb7f67c8647cf63af749361dbc2748c4212fff \
+    Labeling
+}
