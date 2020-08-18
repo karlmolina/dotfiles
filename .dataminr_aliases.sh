e71951eea3f7a2ec97d018644173f334ee080c81
Karl Molina <karlgmolina@gmail.com>
2020-08-18 16:42:42 -0600

    Update dotfiles

--- .dataminr_aliases.sh
+++ .dataminr_aliases.sh
@@ -82,4 +82,4 @@ function hosts() {
 alias report='chrome target/cucumber-html-reports/overview-features.html'
 alias jenkins='chrome http://localhost:49001/'
 # alias local='chrome http://localhost'
-alias sprint='chrome https://jira.dataminr.com/secure/RapidBoard.jspa?rapidView=34&quickFilter=538&quickFilter=117'
+alias sprint='chrome "https://jira.dataminr.com/secure/RapidBoard.jspa?rapidView=34&quickFilter=538&quickFilter=117"'
